7b145c82bdc9c765b80e07634056d257ac908dc1
BenAhmedAmir <benahmedamir@hotmail.fr>
2024-11-27 15:24:28 +0100

    add port number like post service

--- search-service/server.js
+++ search-service/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const searchRoutes = require('./searchAPI');
 const app = express();
-const PORT = 3000;
+const PORT = (+process.env.PORT) || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
